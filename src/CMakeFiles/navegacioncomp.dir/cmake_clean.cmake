fdffe48b540aa53daa2335432395df476578fbdf
salabeta <jbaezami@gmail.com>
2014-09-23 16:02:46 +0200

    proyecto robotica 2014

---
+++ src/CMakeFiles/navegacioncomp.dir/cmake_clean.cmake
@@ -0,0 +1,36 @@
+FILE(REMOVE_RECURSE
+  "CommonBehavior.cpp"
+  "CommonBehavior.h"
+  "Laser.cpp"
+  "Laser.h"
+  "DifferentialRobot.cpp"
+  "DifferentialRobot.h"
+  "moc_specificworker.cxx"
+  "moc_specificmonitor.cxx"
+  "moc_genericmonitor.cxx"
+  "moc_commonbehaviorI.cxx"
+  "moc_genericworker.cxx"
+  "CMakeFiles/navegacioncomp.dir/specificworker.cpp.o"
+  "CMakeFiles/navegacioncomp.dir/specificmonitor.cpp.o"
+  "CMakeFiles/navegacioncomp.dir/navegacioncomp.cpp.o"
+  "CMakeFiles/navegacioncomp.dir/genericmonitor.cpp.o"
+  "CMakeFiles/navegacioncomp.dir/commonbehaviorI.cpp.o"
+  "CMakeFiles/navegacioncomp.dir/genericworker.cpp.o"
+  "CMakeFiles/navegacioncomp.dir/home/salabeta/robocomp/classes/rapplication/rapplication.cpp.o"
+  "CMakeFiles/navegacioncomp.dir/home/salabeta/robocomp/classes/qlog/qlog.cpp.o"
+  "CMakeFiles/navegacioncomp.dir/CommonBehavior.cpp.o"
+  "CMakeFiles/navegacioncomp.dir/Laser.cpp.o"
+  "CMakeFiles/navegacioncomp.dir/DifferentialRobot.cpp.o"
+  "CMakeFiles/navegacioncomp.dir/moc_specificworker.cxx.o"
+  "CMakeFiles/navegacioncomp.dir/moc_specificmonitor.cxx.o"
+  "CMakeFiles/navegacioncomp.dir/moc_genericmonitor.cxx.o"
+  "CMakeFiles/navegacioncomp.dir/moc_commonbehaviorI.cxx.o"
+  "CMakeFiles/navegacioncomp.dir/moc_genericworker.cxx.o"
+  "../bin/navegacioncomp.pdb"
+  "../bin/navegacioncomp"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/navegacioncomp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
